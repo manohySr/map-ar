c2c944389add83f8a73085d8791c3c387c433a13
manohySr <manohyrajaonah@gmail.com>
2024-08-12 18:12:46 +0400

    WIP: itinerary from where I am actually

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+
+const headerSearch = {
+  position: "absolute",
+  top: "0",
+  left: "3rem",
+  width: "80%",
+  zIndex: "1000",
+};
 
 function App() {
   const [origin, setOrigin] = useState(null);
@@ -8,15 +16,37 @@ function App() {
   const [zoom, setZoom] = useState(13);
   const [route, setRoute] = useState(null);
 
+  useEffect(() => {
+    // Get the user's current location when the component mounts
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          const userPosition = [latitude, longitude];
+          setOrigin(userPosition);
+          setPosition(userPosition);
+        },
+        (error) => {
+          console.error(`Error Code = ${error.code}: ${error.message}`);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   return (
     <>
-      <Search
-        setOrigin={setOrigin}
-        setDestination={setDestination}
-        setPosition={setPosition}
-        setZoom={setZoom}
-        setRoute={setRoute}
-      />
+      <div style={headerSearch}>
+        <Search
+          origin={origin}
+          setDestination={setDestination}
+          setPosition={setPosition}
+          setZoom={setZoom}
+          setRoute={setRoute}
+        />
+      </div>
       <MapComponent
         position={position}
         zoom={zoom}
@@ -38,7 +68,7 @@ import {
   Polyline,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 
 function MapComponent({ position, zoom, origin, destination, route }) {
   const mapRef = useRef(null);
@@ -53,7 +83,7 @@ function MapComponent({ position, zoom, origin, destination, route }) {
     <MapContainer
       center={position}
       zoom={zoom}
-      style={{ height: "90vh", width: "100vw" }}
+      style={{ height: "100vh", width: "100vw" }}
       whenReady={(map) => {
         mapRef.current = map.target;
       }}
@@ -64,7 +94,7 @@ function MapComponent({ position, zoom, origin, destination, route }) {
       />
       {origin && (
         <Marker position={origin}>
-          <Popup>Origin</Popup>
+          <Popup>Current Location</Popup>
         </Marker>
       )}
       {destination && (
@@ -85,45 +115,44 @@ const styleSearch = {
 };
 
 const styleInput = {
-  padding: "0 .5rem",
-  fontSize: "1rem",
+  borderRadius: "8px",
+  border: "1px solid #213547",
+  padding: "0.8em 1.2em",
+  fontSize: "1em",
+  background: "white",
+  color: "#000"
 };
 
-function Search({ setOrigin, setDestination, setPosition, setZoom, setRoute }) {
-  const [originSearch, setOriginSearch] = useState("");
+function Search({ origin, setDestination, setPosition, setZoom, setRoute }) {
   const [destinationSearch, setDestinationSearch] = useState("");
   const [distance, setDistance] = useState(null);
 
   async function handleSearch() {
+    if (!origin) {
+      alert("Current location is not available.");
+      return;
+    }
+
     try {
-      const originResponse = await fetch(
-        `https://nominatim.openstreetmap.org/search?format=json&q=${originSearch}`
-      );
-      const originData = await originResponse.json();
       const destinationResponse = await fetch(
         `https://nominatim.openstreetmap.org/search?format=json&q=${destinationSearch}`
       );
       const destinationData = await destinationResponse.json();
 
-      if (originData.length > 0 && destinationData.length > 0) {
-        const newOrigin = [
-          parseFloat(originData[0].lat),
-          parseFloat(originData[0].lon),
-        ];
+      if (destinationData.length > 0) {
         const newDestination = [
           parseFloat(destinationData[0].lat),
           parseFloat(destinationData[0].lon),
         ];
 
-        setOrigin(newOrigin);
         setDestination(newDestination);
-        setPosition(newOrigin);
-        setZoom(13); // Adjust zoom level when new locations are found
+        setPosition(newDestination);
+        setZoom(13); // Adjust zoom level when a new location is found
 
         const routeResponse = await fetch(
           `https://api.openrouteservice.org/v2/directions/foot-walking?api_key=${
             import.meta.env.VITE_API_KEY
-          }&start=${newOrigin[1]},${newOrigin[0]}&end=${newDestination[1]},${
+          }&start=${origin[1]},${origin[0]}&end=${newDestination[1]},${
             newDestination[0]
           }`
         );
@@ -152,13 +181,6 @@ function Search({ setOrigin, setDestination, setPosition, setZoom, setRoute }) {
 
   return (
     <div style={styleSearch}>
-      <input
-        style={styleInput}
-        type="text"
-        value={originSearch}
-        onChange={(e) => setOriginSearch(e.target.value)}
-        placeholder="Origin..."
-      />
       <input
         style={styleInput}
         type="text"
